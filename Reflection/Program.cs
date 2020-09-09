fa0e4166e72614078a23d8d633808ce3f349a6bb
Vladyslav <0965142838gvs@gmail.com>
2020-09-09 20:25:30 +0300

    Add project files.

---
+++ Reflection/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace Reflection
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Input name:");
+            string userName = Console.ReadLine();
+
+            Console.WriteLine("Input age:");
+            int.TryParse(Console.ReadLine(), out int age);
+
+            User user = new User(age, userName);
+
+            Console.WriteLine("Ready!");
+        }
+    }
+
+    public class User
+    {
+        private int _age;
+        private string _name;
+
+        public User(int age, string name)
+        {
+            _age = age;
+            _name = name;
+        }
+
+        public string Name
+        {
+            get { return _name; }
+            set { _name = value; }
+        }
+
+        public int Age
+        {
+            get { return _age; }
+            set { _age = value; }
+        }
+    }
+}
